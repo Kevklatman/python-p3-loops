9913d5b22354acaf9bde8bfa5d27765e25db3bfa
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-25 23:32:56 -0600

    lab complete

--- lib/looping.py
+++ lib/looping.py
@@ -13,5 +13,17 @@ def square_integers(int_list):
     return squares
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i < 101:
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
+
+fizzbuzz()
+    
