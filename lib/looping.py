ccce5bc420c0df8ae4c57f8cc69c21fea4d5c4e7
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-25 23:23:26 -0600

    integer integer integer integer

--- lib/looping.py
+++ lib/looping.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print (f'{i}')
+        i -= 1
+    else:
+        print ("Happy New Year!") 
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squares = [integer * integer for integer in int_list]
+    return squares
 
 def fizzbuzz():
     # code goes here!
